d90a5b3e9fb895e5eb6f06dab5935b3ea33949a5
Tyler Newman <tyler.newman@runtriz.com>
2016-04-26 18:16:39 -0700

    fix mistake, delete wrong file

--- addon/components/smart-banner.js
+++ addon/components/smart-banner.js
@@ -1,12 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  show: true,
-  classNameBindings: ['smart-banner'],
   title: '',
   description: '',
   buttonText: '',
   imgUrl: '',
-  bannerState: 'idle',
-  isLoading: computed.equal('bannerState', 'loading'),
+  showBanner: '',
 });
