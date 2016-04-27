761007721f5f4a9f0e77140cef8da7be1ed0f786
Tyler Newman <tyler.newman@runtriz.com>
2016-04-27 15:44:06 -0700

    remove code from component and place in addon

--- addon/components/smart-banner.js
+++ addon/components/smart-banner.js
@@ -1,5 +1,13 @@
-import Ember from 'ember';
-
 export default Ember.Component.extend({
-
+  classNames: ['ember-smart-banner'],
+  // http://discuss.emberjs.com/t/best-practices-accessing-app-config-from-addon-code/7006/16
+  config: Ember.computed(function() {
+    return Ember.getOwner(this).resolveRegistration('config:environment').emberSmartBanner;
+  }),
+  title: Ember.computed.alias('config.title'),
+  description: Ember.computed.alias('config.description'),
+  buttonText: Ember.computed.alias('config.buttonText'),
+  imgUrl: Ember.computed.alias('config.imgUrl'),
+  showBanner: Ember.computed.or('config.showBanner', 'showBannerDefault'),
+  showBannerDefault: true
 });
--- app/components/smart-banner.js
+++ app/components/smart-banner.js
@@ -1,11 +1,1 @@
-export default Ember.Component.extend({
-  // http://discuss.emberjs.com/t/best-practices-accessing-app-config-from-addon-code/7006/16
-  config: Ember.computed(function() {
-    return Ember.getOwner(this).resolveRegistration('config:environment').emberSmartBanner;
-  }),
-  title: Ember.computed.alias('config.title'),
-  description: Ember.computed.alias('config.description'),
-  buttonText: Ember.computed.alias('config.buttonText'),
-  imgUrl: Ember.computed.alias('config.imgUrl'),
-  showBanner: Ember.computed.alias('config.showBanner'),
-});
+export { default } from 'ember-smart-banner/components/smart-banner';
