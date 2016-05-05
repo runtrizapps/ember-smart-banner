495555b0bceed5b0dda711d3eabec7829dc7cbf7
Tyler Newman <tyler.newman@runtriz.com>
2016-05-05 16:29:02 -0700

    refactor variable names, it does not render on IOS unless iOS platform detected

--- tests/integration/components/ember-smart-banner-test.js
+++ tests/integration/components/ember-smart-banner-test.js
@@ -5,16 +5,7 @@ moduleForComponent('smart-banner', 'Integration | Component | ember smart banner
   integration: true
 });
 
-test('it renders', function(assert) { // TODO - make this more real
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{smart-banner}}`);
-  assert.ok(this.$().text().trim().match(/App Title/));
-});
-
-
-test('it does not render on iOS unless an appId is set', function(assert) {
+test('it renders on iOS when an appId is set', function(assert) {
   this.set('iOS', true);
   this.set('appIdIOS', 123);
   this.set('android', false);
@@ -28,13 +19,28 @@ test('it does not render on iOS unless an appId is set', function(assert) {
     appIdAndroid=appIdAndroid
     appStoreLanguage=appStoreLanguage
   }}`);
-  // TODO Assert that it renders with appStore link
-  assert.equal(this.$('.ember-smart-banner--link').attr('href'), 'https://itunes.apple.com/en/app/123');
-  //
-  // this.set('iosAppId', null);
-  //
-  // // TODO Assert that it is no longer shown
 
+  // Assert that it renders with appStore link
+  assert.equal(this.$('.ember-smart-banner--link').attr('href'), 'https://itunes.apple.com/en/app/123');
+});
+
+test('it does not render on iOS unless an appId is set', function(assert) {
+  this.set('iOS', true);
+  this.set('appIdIOS', null);
+  this.set('android', false);
+  this.set('appIdAndroid', null);
+  this.set('appStoreLanguage', 'en');
+
+  this.render(hbs`{{smart-banner
+    iOS=iOS
+    appIdIOS=appIdIOS
+    android=android
+    appIdAndroid=appIdAndroid
+    appStoreLanguage=appStoreLanguage
+  }}`);
+
+  // Assert that it is no longer shown
+  assert.equal(this.$('.ember-smart-banner--link').length, 0, 'link is not rendered');
 });
 
 test('it does not render on iOS unless iOS platform detected', function(assert) {
@@ -61,35 +67,35 @@ test('it does not render on iOS unless iOS platform detected', function(assert) 
 
 test('it does not render on Android unless appId is set', function(assert) {
   this.set('iOS', false);
-  this.set('iosAppId', null);
+  this.set('appIdIOS', null);
   this.set('android', true);
-  this.set('androidAppId', 123);
+  this.set('appIdAndroid', 123);
 
   this.render(hbs`{{smart-banner
     iOS=iOS
     iOSAppId=iOSAppId
     android=android
-    androidAppId=androidAppId
+    appIdAndroid=appIdAndroid
   }}`);
 
   // TODO Assert that it renders with appStore link
 
-  this.set('androidAppId', null);
+  this.set('appIdAndroid', null);
 
   // TODO Assert that it is no longer shown
 });
 
 test('it does not render on Android unless Android platform detected', function(assert) {
   this.set('iOS', false);
-  this.set('iosAppId', null);
+  this.set('appIdIOS', null);
   this.set('android', true);
-  this.set('androidAppId', 123);
+  this.set('appIdAndroid', 123);
 
   this.render(hbs`{{smart-banner
     iOS=iOS
     iOSAppId=iOSAppId
     android=android
-    androidAppId=androidAppId
+    appIdAndroid=appIdAndroid
   }}`);
 
   // TODO Assert that it renders with appStore link
